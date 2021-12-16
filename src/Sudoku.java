a393fb9e546bc8368ded6d946c4c829b673c1058
Vedant <vedantrajyaguru.333@gmail.com>
2021-12-16 11:03:31 +0530

    input String(cmd) done

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,31 +1,31 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class Sudoku {
 
-    private static boolean isvalid(String[][] sudoku){
-        int i,j;
+    private static boolean isvalid(List<List<String>> sudoku){
+        int row,col;
 
-        for(i=0;i<9;i++){
-            for(j=0;j<9;j++){
-                if(sudoku[i][j].equals(" ")){
+        for(row=0;row<9;row++){
+            for(col=0;col<9;col++){
+                if(sudoku.get(row).get(col).equals(" ")){
                     continue;
                 }
-                else if( (Integer.parseInt(sudoku[i][j]) > 9 && Integer.parseInt(sudoku[i][j]) < 1)) {
+                else if( (Integer.parseInt(sudoku.get(row).get(col)) > 9 && Integer.parseInt(sudoku.get(row).get(col)) < 1)) {
                     return false;
                 }
             }
 
         }
-        for(i=0;i<9;i++){
+        for(row=0;row<9;row++){
             Set<String> set = new HashSet<>();
             int blank_count = 0;
-            for(j=0;j<9;j++){
-                if(sudoku[i][j].equals(" ")){
+            for(col=0;col<9;col++){
+                if(sudoku.get(row).get(col).equals(" ")){
                     blank_count++;
                 }
                 else{
-                    set.add(sudoku[i][j]);
+                    set.add(sudoku.get(row).get(col));
                 }
 
             }
@@ -35,15 +35,15 @@ public class Sudoku {
         }
 
 
-        for(i=0;i<9;i++){
+        for(row=0;row<9;row++){
             Set<String> set = new HashSet<>();
             int blank_count = 0;
-            for(j=0;j<9;j++){
-                if(sudoku[j][i].equals(" ")){
+            for(col=0;col<9;col++){
+                if(sudoku.get(col).get(row).equals(" ")){
                     blank_count++;
                 }
                 else{
-                    set.add(sudoku[j][i]);
+                    set.add(sudoku.get(col).get(row));
                 }
 
             }
@@ -57,19 +57,19 @@ public class Sudoku {
         //cell
         int size_sudoku = 9;
 
-        for(i=0;i<size_sudoku;i+=3){
-            for(j=0;j<size_sudoku;j+=3){
+        for(row=0;row<size_sudoku;row+=3){
+            for(col=0;col<size_sudoku;col+=3){
                 int cell_row,cell_col;
                 Set<String> set = new HashSet<>();
                 int blank_count = 0;
 
                 for(cell_row=0;cell_row<3;cell_row++){
                     for(cell_col=0;cell_col<3;cell_col++){
-                        if(sudoku[i+cell_row][j+cell_col].equals(" ")){
+                        if(sudoku.get(row+cell_row).get(col+cell_col).equals(" ")){
                             blank_count++;
                         }
                         else{
-                            set.add(sudoku[i+cell_row][j+cell_col]);
+                            set.add(sudoku.get(row+cell_row).get(col+cell_col));
                         }
 
                     }
@@ -100,20 +100,45 @@ public class Sudoku {
                 {"1","2","3","4","5","6","7","8","9"}
 
         };
-*/
-        String sudoku[][]={
+
+         String sudoku[][]={
                 {"5","3"," "," ","7"," "," "," "," "},
                 {"6"," "," ","1","9","5"," "," "," "},
                 {" ","9","8"," "," "," "," ","6"," "},
                 {"8"," "," "," ","6"," "," "," ","3"},
                 {"4"," "," ","8"," ","3"," "," ","1"},
                 {"7"," "," "," ","2"," "," "," ","6"},
-                {" ","6"," "," "," "," ","2","8"," "},
+                {" ","6"," "," "," "," "," ","8"," "},
                 {" "," "," ","4","1","9"," "," ","5"},
                 {" "," "," "," ","8"," "," ","7","9"}
 
         };
-        if(isvalid(sudoku)){
+
+*/
+        
+        
+        Scanner s = new Scanner(System.in);
+        List<List<String>> sudoku = new ArrayList<>();
+        System.out.println("Enter 9*9 sudoku :: ");
+        /* input
+53  7    
+6  195   
+ 98    6 
+8   6   3
+4  8 3  1
+7   2   6
+ 6     8 
+   419  5
+    8  79
+    
+*/
+            for (int i=0;i<9;i++){
+            String temp = s.nextLine();
+
+            List<String> row = Arrays.asList(temp.split(""));
+            sudoku.add(row);
+        }
+           if(isvalid(sudoku)){
             System.out.println("Sudoku is Valid!");
         }
         else{
